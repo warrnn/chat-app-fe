37622d8cbee3fe4fece8a1e875e4fe627db8d29d
Bryan Warren <bryanwarren86@gmail.com>
2026-04-29 00:09:45 +0700

    set up socket io client

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@ import MessageField from './components/message-field.vue';
     <header>
       <h1 class="text-2xl text-white">Chat App</h1>
     </header>
-    <section class="flex-1 overflow-y-auto py-4">
-
-    </section>
+    <section class="flex-1 overflow-y-auto py-4 my-4"></section>
     <MessageField />
   </BaseLayout>
 </template>
