179d6bb7629a47e7b1b985852e25c64551925554
Bryan Warren <bryanwarren86@gmail.com>
2026-04-29 16:50:44 +0700

    adding event listener for chat

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+import { socket } from '@/socket';
 import BaseLayout from './components/base-layout.vue';
 import MessageField from './components/message-field.vue';
+
+const messages = ref([]);
+
+// Listen for event
+socket.on('chat', function (data) {
+  messages.value.push({
+    handle: data.handle,
+    message: data.message
+  });
+});
 </script>
 
 <template>
@@ -8,7 +20,11 @@ import MessageField from './components/message-field.vue';
     <header>
       <h1 class="text-2xl text-white">Chat App</h1>
     </header>
-    <section class="flex-1 overflow-y-auto py-4 my-4"></section>
+    <section id="output" class="flex-1 overflow-y-auto py-4 my-4">
+      <div v-for="(msg, index) in messages" :key="index" class="text-white mb-2">
+        <strong>{{ msg.handle }}:</strong> {{ msg.message }}
+      </div>
+    </section>
     <MessageField />
   </BaseLayout>
 </template>
